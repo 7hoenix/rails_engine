94e0ac35937f65990859da4a50ec7a8c8739bbcb
philmphoenix <jpholzmann@gmail.com>
2015-11-03 15:32:06 -0700

    Adds random method.

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,5 @@ class Item < ActiveRecord::Base
   has_many :invoice_items
   has_many :invoices, through: :invoice_items
 
-  scope :random, -> { find((1..Item.count).to_a.sample) }
+  scope :random, -> { order("RANDOM()").first }
 end
