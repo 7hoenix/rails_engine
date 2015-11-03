e750dc8499bca878435507f8722065ba3ae6703f
philmphoenix <jpholzmann@gmail.com>
2015-11-02 19:14:57 -0700

    Adds find feature for invoice items
    Now an api endpoint can hit the find action with any valid
    paremeters and return the first possible invoice item.

--- app/controllers/api/v1/invoice_items_controller.rb
+++ app/controllers/api/v1/invoice_items_controller.rb
@@ -8,4 +8,14 @@ class Api::V1::InvoiceItemsController < ApplicationController
   def show
     respond_with InvoiceItem.find(params[:id])
   end
+
+  def find
+    respond_with InvoiceItem.find_by(invoice_item_params)
+  end
+
+  private
+
+  def invoice_item_params
+    params.permit(:item_id, :invoice_id, :id, :quantity, :unit_price)
+  end
 end
