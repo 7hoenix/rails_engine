03cf84558ab02d3c09be8a3cb59d198628132963
philmphoenix <jpholzmann@gmail.com>
2015-11-04 15:06:01 -0700

    Adds merchants and transactions to invoice.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,7 +1,9 @@
 class InvoiceItem < ActiveRecord::Base
   belongs_to :invoice
   belongs_to :item
+  has_many :merchants, through: :invoice
+  has_many :transactions, through: :invoice
 
   scope :random, -> { order("RANDOM()").first }
-  scope :revenue, -> { quantity * unit_price }
+  scope :created_on, ->(date) { joins(:invoices).where(invoices: { created_at: date }) }
 end
