f3f3a79fab456f5d1cf55040b24c143cff1f5204
philmphoenix <jpholzmann@gmail.com>
2015-11-04 15:05:16 -0700

    Adds relationship for merchant through invoices.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,7 @@
 class Customer < ActiveRecord::Base
   has_many :invoices
   has_many :transactions, through: :invoices
+  has_many :merchants, through: :invoices
 
   scope :random, -> { order("RANDOM()").first }
 end
