1f487e8bc953eb1ee25f2fde0bd8e0199d45a91f
philmphoenix <jpholzmann@gmail.com>
2015-11-04 15:06:55 -0700

    Adds parens... maybe doing nothing.

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -2,5 +2,5 @@ class Transaction < ActiveRecord::Base
   belongs_to :invoice
 
   scope :random, -> { order("RANDOM()").first }
-  scope :successful, -> { where(result: "success") }
+  scope :successful, -> { where result: "success" }
 end
