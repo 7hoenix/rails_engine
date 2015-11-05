9c9019da289c878ab813fcbea34a9709eb2fc735
philmphoenix <jpholzmann@gmail.com>
2015-11-04 22:37:23 -0700

    Fixes price for spec harness and adds successul scope.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,6 @@
 class InvoiceItem < ActiveRecord::Base
+  before_create :correct_unit_price
+
   belongs_to :invoice
   belongs_to :item
   has_many :merchants, through: :invoice
@@ -6,4 +8,9 @@ class InvoiceItem < ActiveRecord::Base
 
   scope :random, -> { order("RANDOM()").first }
   scope :created_on, ->(date) { joins(:invoices).where(invoices: { created_at: date }) }
+  scope :successful, -> { joins(:transactions).where("transactions.result" => "success") }
+
+  def correct_unit_price
+    self.unit_price = unit_price / 100.00
+  end
 end
